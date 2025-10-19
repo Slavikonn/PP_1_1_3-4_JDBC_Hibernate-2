8f957cca68c6a1f85ccb178bd23b734d5d59e1db
Slavikonn <slavikon132@gmail.com>
2025-10-19 17:24:35 +0300

    add Hibernate connection and implement UserDao methods

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,112 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final SessionFactory sessionFactory =
+            Util.getInstance().getSessionFactory();
+
     public UserDaoHibernateImpl() {
-
     }
-
 
     @Override
     public void createUsersTable() {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS users (
+                    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                    name VARCHAR(40) NOT NULL,
+                    last_name VARCHAR(40) NOT NULL,
+                    age TINYINT UNSIGNED NOT NULL)
+                """;
 
+        Transaction transaction = null;
+
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.createNativeQuery(sql).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        String sql = "DROP TABLE IF EXISTS users";
 
+        Transaction transaction = null;
+
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.createNativeQuery(sql).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
 
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        String hql = "DELETE FROM User WHERE id = :id";
 
+        Transaction transaction = null;
+
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.createQuery(hql)
+                    .setParameter("id", id).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        String hql = "FROM User";
+
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery(hql, User.class).list();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
+        String sql = "TRUNCATE TABLE users";
 
+        Transaction transaction = null;
+
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.createNativeQuery(sql).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,15 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static volatile Util instance;
+    private volatile SessionFactory sessionFactory;
 
     private static final String URL = "jdbc:mysql://localhost:3306/jdbc_hibernate";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DIALECT = "org.hibernate.dialect.MySQL8Dialect";
+    private static final String SHOW_SQL = "true";
 
     private Util() {
     }
@@ -25,10 +35,46 @@ public class Util {
         return instance;
     }
 
+
+    // JDBC connection
+
     public Connection getConnection() {
         try {
             return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    // Hibernate connection
+
+    public SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            synchronized (Util.class) {
+                if (sessionFactory == null) {
+                    sessionFactory = buildSessionFactory();
+                }
+            }
+        }
+        return sessionFactory;
+    }
+
+    private SessionFactory buildSessionFactory() {
+        try {
+            Properties properties = new Properties();
+            properties.setProperty(Environment.DRIVER, DRIVER);
+            properties.setProperty(Environment.URL, URL);
+            properties.setProperty(Environment.USER, USERNAME);
+            properties.setProperty(Environment.PASS, PASSWORD);
+            properties.setProperty(Environment.DIALECT, DIALECT);
+            properties.setProperty(Environment.SHOW_SQL, SHOW_SQL);
+
+            return new Configuration()
+                    .addProperties(properties)
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
